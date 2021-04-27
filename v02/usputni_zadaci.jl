7dff3eb4d7ff909a7c179ecc34e0496ce05cf97a
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-04-27 13:42:12 +0200

    zagrevanje

---
+++ v02/usputni_zadaci.jl
@@ -0,0 +1,12 @@
+# obim pravougaonika
+function obimPravougaonika(a, b)
+	return 2*(a+b)
+end
+
+# zbir, razlika i proizvod dve matrice
+function matrice(A, B)
+	C=A+B
+	D=A-B
+	E=A*B
+	return C, D, E
+end
