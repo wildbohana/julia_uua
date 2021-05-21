f4fa441dc1b88294015d140a06858b41a9ae17c7
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-05-22 01:12:08 +0200

    dfs + grupe

--- t34_z41/z41.jl
+++ t34_z41/z41.jl
@@ -6,7 +6,7 @@ function DFS!(G)
         G.V[u].color = 'W'
         G.V[u].pred = -1
     end
-    bro=0						# broj ostrva
+    bro=0					# broj ostrva
     for u in v
         if G.V[u].color == 'W'
             bro+=1
@@ -30,25 +30,25 @@ end
 
 function grupe(G)
 	v=1:size(G.AdjMatrix, 1)
-	pamti=[]					# ovde se smešta šta mi treba
+	pamti=[]				# ovde se smešta šta mi treba
 	for u in v
 		if findall(pamti.==G.V[u].d) == []
 			push!(pamti, u)
 		end
 	end
-	   				# dobiju se neke loše vrednosti, nisu nam bitne, bitan nam je broj grupa samo
-	   				# samo setuješ ceo taj niz na nulu
+	   					# dobiju se neke loše vrednosti, nisu nam bitne, bitan nam je broj grupa samo
+	   					# samo setuješ ceo taj niz na nulu
 	for i in 1:length(pamti)
 		pamti[i]=0
 	end
 	for u in v
 		pamti[(G.V[u].d)]+=1
-	end				# d će u sebi imati broj grupe (1-4) a i niz pamti ima 4 elementa
-					# što znači da se broj grupe može koristiti za pristup elementima iz pamti
-	  				# i to iskoristimo tako što ćemo u pamti[1] pamtiti koliko el ima 
-					# u prvoj grupi, u pamti[2] koliko elemenata ima u grugoj itd
-					# tako što će nam G.V[u].d biti indeks kom elementu iz pamti[] pristupamo
-					# i taj element po redu ćemo da povećamo za 1 (zato se počinje od 0)
+	end					# d će u sebi imati broj grupe (1-4) a i niz pamti ima 4 elementa
+						# što znači da se broj grupe može koristiti za pristup elementima iz pamti
+	  					# i to iskoristimo tako što ćemo u pamti[1] pamtiti koliko el ima 
+						# u prvoj grupi, u pamti[2] koliko elemenata ima u grugoj itd
+						# tako što će nam G.V[u].d biti indeks kom elementu iz pamti[] pristupamo
+						# i taj element po redu ćemo da povećamo za 1 (zato se počinje od 0)
 	return pamti
 end
 
